131f0208adca351c4737d8fdf5dd40cff2d360b8
ayo <kaweapplication@gmail.com>
2020-01-03 16:00:38 +0100

    Updates

---
+++ precache-manifest.69104ef7b64f0a8a572db257a6261865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724d1046854dd378e5705929a40ba085",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "06157e83c7e574d0f8fb",
+    "url": "/calendar/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e853a09d8fcedb267f73",
+    "url": "/calendar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "06157e83c7e574d0f8fb",
+    "url": "/calendar/static/js/2.fbc4ab91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calendar/static/js/2.fbc4ab91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e853a09d8fcedb267f73",
+    "url": "/calendar/static/js/main.82fb09e7.chunk.js"
+  },
+  {
+    "revision": "d63e43fb8d7cb705ffa2",
+    "url": "/calendar/static/js/runtime-main.fa82e80b.js"
+  }
+]);
